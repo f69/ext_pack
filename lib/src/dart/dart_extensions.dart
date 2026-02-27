f89fb0e30888d67fc0052bee35f3516ce8bb2f82
f69 <f69@w>
2026-02-27 19:15:35 +0300

    Corrects exports

--- lib/src/dart/dart_extensions.dart
+++ lib/src/dart/dart_extensions.dart
@@ -1,4 +1,5 @@
 export 'date_ext.dart';
+export 'double_ext.dart';
 export 'duration_ext.dart';
 export 'int_ext.dart';
 export 'iterable_ext.dart';
--- lib/src/flutter/flutter_extensions.dart
+++ lib/src/flutter/flutter_extensions.dart
@@ -2,3 +2,4 @@ export 'build_context/media_query_ext.dart';
 export 'build_context/messenger_ext.dart';
 export 'build_context/navigator_ext.dart';
 export 'build_context/theme_ext.dart';
+export 'num_ext.dart';
