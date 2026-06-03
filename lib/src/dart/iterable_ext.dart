316cda39ab306cb35139cf42f4c1473ff74e2e1b
f69 <f69@w>
2026-06-03 18:23:01 +0300

    Adds safeLength iterable property

--- lib/src/dart/iterable_ext.dart
+++ lib/src/dart/iterable_ext.dart
@@ -5,4 +5,7 @@ extension NullIterableExt<T> on Iterable<T>? {
 
   /// Whether this iterable is not empty
   bool get isNotBlank => !isBlank;
+
+  /// The number of elements in this [Iterable], or 0 if it is null
+  int get safeLength => this == null ? 0 : this!.length;
 }
